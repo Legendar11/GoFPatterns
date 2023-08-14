a7a0b0b28d80bf74f57aee692e40e8ca7d9471f1
valeriy <vp@cteleport.com>
2023-08-14 22:06:30 +0200

    implement singleton

--- src/Singleton/Program.cs
+++ src/Singleton/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Singleton;
+
+// Gang of Four, Singleton:
+// It restricts a class to have only one instance
+// and provides a global point of access to that instance.
+// The singleton class is responsible for creating its own instance,
+// and making sure that only one instance is created.
+
+//// creation date will be set immediately
+//var eagerInstance = World.GetInstance();
+//Console.WriteLine(eagerInstance.CreationDate);
+
+//Thread.Sleep(5000);
+
+//// creation date will be created only after request the lazy instance
+//var lazyInstance = World.GetLazyInstance();
+//Console.WriteLine(lazyInstance.CreationDate);
+
+
+
+// we requested a static property, so 
+// eagerInstance.CreationDate will be set as well
+var lazyInstance = World.GetLazyInstance();
+Console.WriteLine(lazyInstance.CreationDate);
+
+Thread.Sleep(5000);
+
+var eagerInstance = World.GetInstance();
+Console.WriteLine(eagerInstance.CreationDate);
---
+++ src/Singleton/World.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton;
+
+internal class World
+{
+    // Will be initiated only by demand
+    private static readonly Lazy<World> _lazyInstance = new(() => new World());
+
+    // Will be initiated after first call to any static property and before static constructor
+    private static readonly World _instance = new World();
+
+    public DateTime CreationDate { get; } = DateTime.Now;
+
+    private World()
+    {
+    }
+
+    public static World GetLazyInstance()
+    {
+        return _lazyInstance.Value;
+    }
+
+    public static World GetInstance()
+    {
+        return _instance;
+    }
+}
