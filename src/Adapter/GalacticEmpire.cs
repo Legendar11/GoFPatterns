823b926165d6aa8686b6b7bf02527d7e3772e077
valeriy <vp@cteleport.com>
2023-08-15 19:08:56 +0200

    implemented adapter

---
+++ src/Adapter/GalacticEmpire.cs
@@ -0,0 +1,37 @@
+﻿namespace Adapter;
+
+internal class GalacticEmpire
+{
+    // Galactic Empire has Tatooin planet service as is
+    private readonly TatooineService _tatooineService;
+
+    public GalacticEmpire()
+    {
+        _tatooineService = new TatooineService(population: 250_000);
+
+        _tatooineService.AddExported(1000);
+        _tatooineService.AddImported(355);
+    }
+
+    public override string ToString()
+    {
+        // Galactic Empire talks with Tatooine only through special adapter because:
+        // 1. Tatooine has too unsafe, unstable and old interface to communication,
+        // 2. Galactic Empire talks to planets by uniform standard language,
+        // 3. Adapter guarantees to provide safety data by standard way.
+
+        var adapter = new TatooineServiceAdapter(_tatooineService);
+        const string noData = "no data";
+
+        var population = adapter.GetPopulation();
+        var commerceIndex = adapter.GetCommerceIndex();
+        var povertyIndex = adapter.GetPovertyIndex();
+
+        return $"""
+            Tatooine has:
+                Population: {population?.ToString() ?? noData}
+                Commerce index: {(commerceIndex is not null ? string.Format("{0:f4}", commerceIndex) : noData)}
+                Poverty index: {(povertyIndex is not null ? string.Format("{0:f4}", povertyIndex) : noData)}
+        """;
+    }
+}
--- src/Adapter/Program.cs
+++ src/Adapter/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Adapter;
+
+// Gang of Four, Adapter:
+// An adapter allows two incompatible interfaces to work together.
+// This is the real-world definition for an adapter.
+// Interfaces may be incompatible, but the inner functionality should suit the need.
+// The adapter design pattern allows otherwise incompatible classes to work together
+// by converting the interface of one class into an interface expected by the clients.
+
+// Galactic Empire has Tatooine planet.
+// They need to establish communication.
+// Galactice Empire needs to special adapter to contact with Tatooine.
+
+// Galactic Empire - Client,
+// Tatooine - Service with incompatible interface,
+// TatooineAdapter - Adapter with expected interface.
+
+var empire = new GalacticEmpire();
+
+Console.WriteLine(empire);
